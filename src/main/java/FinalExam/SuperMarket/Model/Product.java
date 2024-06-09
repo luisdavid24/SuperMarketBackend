a033c3bc41eb6dbb8ae6bc941265479d9b6d3a53
Luis David <david12.29@hotmail.com>
2024-06-09 17:53:01 -0500

    Refactor, Product class is finish

--- src/main/java/FinalExam/SuperMarket/Model/Product.java
+++ src/main/java/FinalExam/SuperMarket/Model/Product.java
@@ -1,5 +1,24 @@
 package FinalExam.SuperMarket.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
 public class Product {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String category;
 }
